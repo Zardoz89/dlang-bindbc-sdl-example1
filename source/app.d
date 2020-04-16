1d65b972353ed2d602dda1da58e3075b54e68719
Luis Panadero Guardeño <luis.panadero@gmail.com>
2020-04-16 20:13:52 +0200

    Converted to use bindbc-sdl

--- source/app.d
+++ source/app.d
@@ -1,89 +1,97 @@
 import std.stdio;
-import derelict.sdl2.sdl;
-import derelict.sdl2.image;
 import std.conv;
+
+import bindbc.sdl;
+import bindbc.sdl.image;
 
 void main()
 {
-	DerelictSDL2.load();
-	DerelictSDL2Image.load();
+    const SDLSupport ret = loadSDL();
+    if(ret != sdlSupport) {
+      writeln("Error loading SDL dll");
+      return;
+    }
+    if(loadSDLImage() != sdlImageSupport) {
+      writeln("Error loading SDL Image dll");
+      return;
+    }
 
-	//  padding around image in pixels
-	int padding = 20;
+    //  padding around image in pixels
+    int padding = 20;
 
-	// Initialise SDL
-	if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-		writeln("SDL_Init: ", SDL_GetError());
-	}
+    // Initialise SDL
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        writeln("SDL_Init: ", SDL_GetError());
+    }
 
-	// Initialise IMG
-	int flags = IMG_INIT_PNG | IMG_INIT_JPG;
-	if ((IMG_Init(flags) & flags) != flags) {
-		writeln("IMG_Init: ", to!string(IMG_GetError()));
-	}
+    // Initialise IMG
+    int flags = IMG_INIT_PNG | IMG_INIT_JPG;
+    if ((IMG_Init(flags) & flags) != flags) {
+        writeln("IMG_Init: ", to!string(IMG_GetError()));
+    }
 
-	// Load image
-	SDL_Surface *imgSurf = IMG_Load("grumpy-cat.jpg");
-	if (imgSurf is null) {
-		writeln("IMG_Load: ", to!string(IMG_GetError()));
-	}
+    // Load image
+    SDL_Surface *imgSurf = IMG_Load("grumpy-cat.jpg");
+    if (imgSurf is null) {
+        writeln("IMG_Load: ", to!string(IMG_GetError()));
+    }
 
-	// Create a window
-	SDL_Window* appWin = SDL_CreateWindow(
-		"Example #1",
-		SDL_WINDOWPOS_UNDEFINED,
-		SDL_WINDOWPOS_UNDEFINED,
-		imgSurf.w + padding * 2,
-		imgSurf.h + padding * 2,
-		SDL_WINDOW_OPENGL
-	);
-	if (appWin is null) {
-		writefln("SDL_CreateWindow: ", SDL_GetError());
-		return;
-	}
+    // Create a window
+    SDL_Window* appWin = SDL_CreateWindow(
+        "Example #1",
+        SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED,
+        imgSurf.w + padding * 2,
+        imgSurf.h + padding * 2,
+        SDL_WINDOW_OPENGL
+    );
+    if (appWin is null) {
+        writefln("SDL_CreateWindow: ", SDL_GetError());
+        return;
+    }
 
-	// Get the window surface
-	SDL_Surface *winSurf = SDL_GetWindowSurface(appWin);
-	if (winSurf is null) {
-		writeln("SDL_GetWindowSurface: ", SDL_GetError());
-	}
+    // Get the window surface
+    SDL_Surface *winSurf = SDL_GetWindowSurface(appWin);
+    if (winSurf is null) {
+        writeln("SDL_GetWindowSurface: ", SDL_GetError());
+    }
 
-	// Define a colour for the surface, based on RGB values
-	int colour = SDL_MapRGB(winSurf.format, 0xFF, 0xFF, 0xFF);
+    // Define a colour for the surface, based on RGB values
+    int colour = SDL_MapRGB(winSurf.format, 0xFF, 0xFF, 0xFF);
 
-	// Fill the window surface with the colour
-	SDL_FillRect(winSurf, null, colour);
+    // Fill the window surface with the colour
+    SDL_FillRect(winSurf, null, colour);
 
-	// Copy loaded image to window surface
-	SDL_Rect dstRect;
-	dstRect.x = padding;
-	dstRect.y = padding;
-	SDL_BlitSurface(imgSurf, null, winSurf, &dstRect);
+    // Copy loaded image to window surface
+    SDL_Rect dstRect;
+    dstRect.x = padding;
+    dstRect.y = padding;
+    SDL_UpperBlit(imgSurf, null, winSurf, &dstRect);
 
-	// Copy the window surface to the screen
-	SDL_UpdateWindowSurface(appWin);
+    // Copy the window surface to the screen
+    SDL_UpdateWindowSurface(appWin);
 
-	// Polling for events
-	SDL_Event event;
-	bool quit = false;
-	while(!quit) {
-			while (SDL_PollEvent(&event)) {
-				if (event.type == SDL_QUIT) {
-					quit = true;
-				}
+    // Polling for events
+    SDL_Event event;
+    bool quit = false;
+    while(!quit) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                quit = true;
+            }
 
-				if (event.type == SDL_KEYDOWN) {
-					quit = true;
-				}
-			}
-	}
+            if (event.type == SDL_KEYDOWN) {
+                quit = true;
+            }
+        }
+    }
 
-	// Close and destroy the window
-	if (appWin !is null) {
-		SDL_DestroyWindow(appWin);
-	}
+    // Close and destroy the window
+    if (appWin !is null) {
+        SDL_DestroyWindow(appWin);
+    }
 
-	// Tidy up
-	IMG_Quit();
-	SDL_Quit();
+    // Tidy up
+    IMG_Quit();
+    SDL_Quit();
 }
